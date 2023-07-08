6f0eb43382f149d8aa812c91baf4b9bf18a6a986
lcy0x1 <arthurlcy0x1@gmail.com>
2023-07-08 11:02:41 +0800

    fix server

--- src/main/java/dev/xkmc/l2tabs/compat/CuriosEventHandler.java
+++ src/main/java/dev/xkmc/l2tabs/compat/CuriosEventHandler.java
@@ -1,22 +1,29 @@
 package dev.xkmc.l2tabs.compat;
 
-import dev.xkmc.l2tabs.init.L2Tabs;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 import top.theillusivec4.curios.api.event.SlotModifiersUpdatedEvent;
 
-@Mod.EventBusSubscriber(modid = L2Tabs.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class CuriosEventHandler {
 
 	@SubscribeEvent
 	public static void onSlotModifierUpdate(SlotModifiersUpdatedEvent event) {
-		if (event.getEntity() instanceof ServerPlayer player && player.containerMenu instanceof CuriosListMenu list) {
-			ItemStack stack = list.getCarried();
-			list.setCarried(ItemStack.EMPTY);
-			TabCuriosCompat.openCuriosTab(player);
-			player.containerMenu.setCarried(stack);
+		if (event.getEntity() instanceof Player pl) {
+			if (event.getEntity() instanceof ServerPlayer player) {
+				if (player.containerMenu instanceof CuriosListMenu list) {
+					ItemStack stack = list.getCarried();
+					list.setCarried(ItemStack.EMPTY);
+					TabCuriosCompat.openCuriosTab(player);
+					player.containerMenu.setCarried(stack);
+				}
+			} else if (event.getEntity().level().isClientSide()) {
+				if (pl instanceof LocalPlayer && pl.containerMenu instanceof CuriosListMenu list) {
+					list.slots.clear();
+				}
+			}
 		}
 	}
 
--- src/main/java/dev/xkmc/l2tabs/init/L2Tabs.java
+++ src/main/java/dev/xkmc/l2tabs/init/L2Tabs.java
@@ -4,13 +4,17 @@ import com.tterrag.registrate.providers.ProviderType;
 import dev.xkmc.l2library.base.L2Registrate;
 import dev.xkmc.l2library.serial.config.ConfigTypeEntry;
 import dev.xkmc.l2library.serial.config.PacketHandlerWithConfig;
+import dev.xkmc.l2tabs.compat.CuriosEventHandler;
 import dev.xkmc.l2tabs.compat.TabCuriosCompat;
 import dev.xkmc.l2tabs.init.data.*;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.network.NetworkDirection;
+import top.theillusivec4.curios.api.CuriosApi;
 
 @Mod(L2Tabs.MODID)
 @Mod.EventBusSubscriber(modid = L2Tabs.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -30,6 +34,8 @@ public class L2Tabs {
 		L2TabsConfig.init();
 		TabCuriosCompat.onStartup();
 		REGISTRATE.addDataGenerator(ProviderType.LANG, L2TabsLangData::genLang);
+		if (ModList.get().isLoaded(CuriosApi.MODID))
+			MinecraftForge.EVENT_BUS.register(CuriosEventHandler.class);
 	}
 
 	@SubscribeEvent
